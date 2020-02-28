b6e964540c9eefa753cda701d1a497139ac51f69
Robin Jones <RobinJones335@gmail.com>
2020-02-28 16:31:21 -0500

    added styling the score is now on the page

--- quiz.js
+++ quiz.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-    var canswer = 5
-    var lanswer = -10
+    var canswer = 0
+    var lanswer = 0
+    
 var questions = [
     {
         question: "How do you change the background color in css?",
@@ -11,52 +12,63 @@ var questions = [
     {
         question: "what goes at the top of the HTML file?",
         answers: ["!DOCTYPE","html","html!","none of the above"],
-        correctanswer: "!DOCTYPE"
+        correctanswer: "!DOCTYPE",
+        wronganswer: ["html", "html!", "none of the above"],
     },
     {
         question: "what html tag is used to add a link to an html page?",
         answers: ["a","link","h2","none of the above"],
-        correctanswer: "link"
+        correctanswer: "link",
+        wronganswer: ["a", "h2", "none of the above"],
     },
     {
         question: "where do link the javascript file?",
         answers: ["the head","the body","after the body","none of the above"],
-        correctanswer: "after the body"
+        correctanswer: "after the body",
+        wronganswer: ["the head", "the body", "none of the above"],
     },
     {
         question: "what tag is used to style an id in css",
         answers: ["pound sign","html","period","none of the above"],
-        correctanswer: "pound sign"
+        correctanswer: "pound sign",
+        wronganswer: ["html", "period", "none of the above"],
     }
 ]
 var qarea = $('#quizarea');
 var currentQuestion = 0;
+score = 0;
 
 function clickAnswer(){
-    alert ("correct",$(this).attr("data-name"))
+    // alert ("correct",$(this).attr("data-name"))
     var selected = $(this).attr("data-name")
 
+
     if (selected === questions[currentQuestion].correctanswer ) {
+       score = score +5  
         correctAnswer()
+        alert ("Great job you got " + score + "points")
+        document.getElementById("endGame").innerHTML = score + "&nbsp"+"Keep it up";
+
     
     } else {
         wrongAnswer()
-        alert ("incorrect")
+        score = score -5
+        alert ("incorrect you lost 5 points")
+        document.getElementById("endGame").innerHTML = score + "&nbsp"+"oh no";
     }
 } 
 
 
-
 function correctAnswer(){
- canswer ++
  currentQuestion ++
  render()
 }
 
 function wrongAnswer(){
-canswer --
-currentQuestion --
+currentQuestion ++
+render()
 }
+
 
 
 function render() {
@@ -78,25 +90,28 @@ function render() {
 $('#start').on('click', function() {
     alert("Instructions, answer as many questions correctly as you can in 60 seconds!")
 
-    var timeleft = 60;
+    var timeleft = 15;
+    
 
     var downloadTimer = setInterval(function function1(){
         document.getElementById("countdown").innerHTML = timeleft + "&nbsp"+"seconds remaining";
-
+       
         timeleft -= 1;
-        if(timeleft <= 0){
+        if(timeleft + canswer <= 0){
             clearInterval(downloadTimer);
             document.getElementById("countdown").innerHTML ="TIMES UP!!"
+        } else {
+            
+              
         }
+
+        
+            
+        
     }, 1000);
 
-render()   
-//     qarea.html("<h2>" + questions[currentQuestion].question + "</h2>")
-//     for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
-//    qarea.append("<button class='answer-button' id='button' data-name='" + questions[currentQuestion].answers[i]
-//    + "'>" + questions[currentQuestion].answers[i] + "</button>") 
-        
-//     }
+render() 
+
 });
 
 $(document).on('click', ".answer-button", clickAnswer)
